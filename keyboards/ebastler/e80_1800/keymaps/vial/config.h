d8112856baad2e5915f4818f5c86916a85a9605f
moritz plattner <moritz.plattner@gmx.net>
2022-11-30 23:09:26 +0100

    added e80_1800

---
+++ keyboards/ebastler/e80_1800/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x49, 0x96, 0x37, 0xE0, 0x42, 0x21, 0xEF, 0xDC}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+
+#define VIAL_UNLOCK_COMBO_COLS { 4, 14 }

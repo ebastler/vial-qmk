445575f9ed1d6398f245c9f689b37b6be7f6507a
Moritz Plattner <moritz.plattner@gmx.net>
2023-11-07 14:30:29 +0800

    add vial support for geist

---
+++ keyboards/geistmaschine/geist/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA3, 0x70, 0xE5, 0xE8, 0x2E, 0x38, 0x2B, 0xED}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 9 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 7 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3

19385922d4ebfdaeaa00524a146dff0c4ec324c3
Moritz Plattner <moritz.plattner@gmx.net>
2023-11-07 13:46:11 +0800

    add vial keymap

---
+++ keyboards/geistmaschine/macropod/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFE, 0x83, 0xBA, 0x47, 0x28, 0xDC, 0xFF, 0x23}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
